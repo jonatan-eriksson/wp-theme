6ba92a0f82e2ad89882a435476bcc6ffc092083a
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-10-31 20:16:50 +0100

    Add footer
    * Add footer with content to close header tags
    
    * Add bootstrap to loader functions
    
    * Add footer to index page

--- functions.php
+++ functions.php
@@ -1,6 +1,13 @@
 <?php
 function addStyleSheets(): void
 {
+  wp_enqueue_style('bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css", array(), '5.0.0', 'all');
   wp_enqueue_style('style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'addStyleSheets');
+
+function addScripts(): void
+{
+  wp_enqueue_script('bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js", array(), '5.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'addScripts');
