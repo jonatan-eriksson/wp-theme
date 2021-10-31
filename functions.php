3b8cd15227a86500943d9efa5472570645486559
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-10-31 20:59:52 +0100

    Add menu support

--- functions.php
+++ functions.php
@@ -11,3 +11,13 @@ function addScripts(): void
   wp_enqueue_script('bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js", array(), '5.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'addScripts');
+
+function addMenus()
+{
+  $locations = array(
+    'header' => "Header menu",
+  );
+
+  register_nav_menus($locations);
+}
+add_action('init', 'addMenus');
