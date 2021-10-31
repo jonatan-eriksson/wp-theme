c47666a120412105ca89c9f6b8f01bc72a25852d
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-10-31 21:05:30 +0100

    Update stylesheet loader

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
 <?php
 function addStyleSheets(): void
 {
+  $version = wp_get_theme()->get('Version');
+  wp_enqueue_style('style', get_stylesheet_uri(), array('bootstrap'), $version, 'all');
   wp_enqueue_style('bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css", array(), '5.0.0', 'all');
-  wp_enqueue_style('style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'addStyleSheets');
 
